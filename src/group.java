3d5b1ba680afaa9a6e97a22670808810c207125f
User <User@DESKTOP-CLUO4DV>
2021-05-08 11:03:01 +0300

    i am new team member

--- src/group.java
+++ src/group.java
@@ -16,6 +16,7 @@ public class group
             System.out.print("Choose the operation you want to perform:");
             System.out.print("Choose the operation you want to perform:");
             System.out.print("Choose the operation you want to perform:");
+            System.out.println("thankyou");
             int n = s.nextInt();
             switch(n)
             {
